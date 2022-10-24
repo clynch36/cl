baeaefba5c346dfdb64840e2084b4b245074a582
clynch36 <catherine@taprecruit.co>
2022-10-24 16:02:01 -0700

    Added percent diff function

--- R/catherines_functions.R
+++ R/catherines_functions.R
@@ -3,9 +3,23 @@
 #' @param num1 first number
 #' @param num2 second number
 #'
-#' @return
+#' @return The percent of change over time
 #' @export
 
 pct_chg <- function(num1, num2){
   ((num2-num1)/num1)*100
+}
+
+
+#' Percent Difference Calculation
+#'
+#' @param num1 one comparison number
+#' @param num2 the other comparison number
+#'
+#' @return The percent of difference between two numbers
+#' @export
+#'
+#' @examples
+pct_diff <- function(num1, num2){
+  (num1-num2)/((num1+num2)/2)*100
 }
